4776c2fa20acaa9d60462ee508b9dc989c70eca8
Yingying Zhu <yizhu@c2r6s5.42madrid.com>
2025-04-18 15:55:35 +0200

    changes

--- functions/ft_calloc.c
+++ functions/ft_calloc.c
@@ -14,5 +14,5 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	j
+	
 }
--- functions/libft.h
+++ functions/libft.h
@@ -52,5 +52,4 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
-
 #endif
